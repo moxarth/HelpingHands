cdeecbcba829c342abd3faa241f97b87db265d9c
Ayush Patel <patel.ayush.h@gmail.com>
2020-04-23 00:42:52 +0530

    Initial commit

--- app/src/main/java/com/example/helpinghands/ui/requests/RequestsFragment.java
+++ app/src/main/java/com/example/helpinghands/ui/requests/RequestsFragment.java
@@ -45,101 +45,13 @@ public class RequestsFragment extends Fragment{
     static Location mlocation;
     LocationManager locationManager;
     private RequestsViewModel requestsViewModel;
-    static String hello = "hmmm";
-
-    public void locationfetch(){
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){}
-
-        mlocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        Log.v("MapLOG","ReqFrg Network: "+mlocation);
-        Toast.makeText(getActivity(),"Last Location: "+mlocation,Toast.LENGTH_SHORT).show();
-        if(mlocation == null){
-            final ProgressDialog progressBar;
-            progressBar = new ProgressDialog(getContext());
-            progressBar.setCancelable(true);
-            progressBar.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                @Override
-                public void onCancel(DialogInterface dialog) {
-                    Intent intent = new Intent(getContext(), MainActivity.class);
-                    startActivity(intent);
-                    getActivity().overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
-                }
-            });
-            progressBar.setMessage("Finding current location...");
-            progressBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            progressBar.setProgress(0);
-            progressBar.setMax(100);
-            progressBar.show();
-            Log.v("MapLOG","Location is null");
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
-                @Override
-                public void onLocationChanged(Location location) {
-                    Log.v("MapLOG","Found in new: "+location.getLatitude()+", "+location.getLongitude());
-                    Toast.makeText(getActivity(),location.getLatitude()+", "+location.getLongitude(),Toast.LENGTH_SHORT).show();
-                    mlocation = location;
-                }
-                @Override public void onStatusChanged(String provider, int status, Bundle extras) {}
-                @Override public void onProviderEnabled(String provider) {}
-                @Override public void onProviderDisabled(String provider) {}
-            });
-            int i=0;
-            while(i<30){
-                i++;
-                if(mlocation != null){break;}
-                else{Log.v("MapLOG","Trying my best: "+i);}
-            }
-            Log.v("MapLOG","Finally: "+mlocation);
-            /*new CountDownTimer(5000,1000){
-                @Override
-                public void onFinish() {
-                    progressBar.dismiss();
-                    Log.v("MapLOG","In the end : "+mlocation);
-                    if(mlocation == null){
-                        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                        builder.setMessage("We could not find your location. Please try again.")
-                                .setCancelable(false)
-                                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
-                                        dialog.cancel();
-                                        Intent intent = new Intent(getActivity(),MainActivity.class);
-                                        startActivity(intent);
-                                        getActivity().overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
-                                    }
-                                });
-                        final AlertDialog alert = builder.create();
-                        alert.show();
-                    }
-                    else{
-                    }
-                }
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    if(mlocation != null){this.cancel();progressBar.dismiss();Log.v("MapLOG",""+mlocation);}
-                    Log.v("MapLOG",""+(millisUntilFinished+1000)/1000);
-                }
-            }.start();*/
-        }
-        else {
-                Log.v("MapLOG","Location = "+mlocation);
-        }
-    }
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         requestsViewModel =
                 ViewModelProviders.of(this).get(RequestsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_requests, container, false);
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){}
 
-        mlocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        Log.v("MapLOG","ReqFrg Network: "+mlocation);
-       // locationfetch();
-        //Log.v("MapLOG","Outside the loop"+mlocation);
         return root;
     }
 

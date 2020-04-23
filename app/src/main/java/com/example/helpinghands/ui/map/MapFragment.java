ed27159aacf1e0ffc83eac4e61b2fc689b0ee17d
Ayush Patel <patel.ayush.h@gmail.com>
2020-04-23 14:30:38 +0530

    Initial commit

--- app/src/main/java/com/example/helpinghands/ui/map/MapFragment.java
+++ app/src/main/java/com/example/helpinghands/ui/map/MapFragment.java
@@ -102,7 +102,7 @@ public class MapFragment extends Fragment {
     static Circle circle;
     private static final String TAG = "MapLOG";
     static int focus = 1;
-    static int testing123;
+    static int updating;
     public MapFragment(){}
 
     private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {

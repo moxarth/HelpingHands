9891d84bc48c6a64adb6f8df74e4bd8fb94374d5
Ayush Patel <patel.ayush.h@gmail.com>
2020-04-25 13:26:14 +0530

    version_1.4

--- app/src/main/java/com/example/helpinghands/BackgroundService.java
+++ app/src/main/java/com/example/helpinghands/BackgroundService.java
@@ -20,6 +20,19 @@ import static androidx.constraintlayout.widget.Constraints.TAG;
 public class BackgroundService extends Service {
     static int counter;
     static FirebaseFirestore db;
+
+    public boolean checkInternetStatus(){
+        boolean status = false;
+        try {
+            final String command = "ping -c 1 google.com";
+            status = (Runtime.getRuntime().exec(command).waitFor() == 0);
+            Log.v(TAG, "Network Status: " + status);
+        } catch (Exception e) {
+            Log.e(TAG,"Network Error: "+e.toString());
+        }
+        return status;
+    }
+
     public LatLng locationfetch(Context context){
         LocationManager locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
         final Location currentLoc;
@@ -44,10 +57,14 @@ public class BackgroundService extends Service {
     public int onStartCommand(final Intent intent, int flags, int startId) {
         User user = new User(getApplicationContext());
         if(counter == 0) {
-            LatLng location = locationfetch(getApplicationContext());
-            user.setLatitude(Double.toString(location.latitude));
-            user.setLongitude(Double.toString(location.longitude));
-            db.collection("user_details").document(user.getUserid()).update("latitude", location.latitude, "longitude", location.longitude);
+            LocationManager locationManager1 = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            if (locationManager1.isProviderEnabled(LocationManager.NETWORK_PROVIDER) && checkInternetStatus() && ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                LatLng location = locationfetch(getApplicationContext());
+                user.setLatitude(Double.toString(location.latitude));
+                user.setLongitude(Double.toString(location.longitude));
+                db.collection("user_details").document(user.getUserid()).update("latitude", location.latitude, "longitude", location.longitude);
+            }
+            else{Log.v("BackgroundService","Something went wrong");}
             counter++;
           }
         return Service.START_REDELIVER_INTENT;
